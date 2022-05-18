8a427be2608903975e71388b912a184811d5b7d9
oji3 <oji3xx@gmail.com>
2022-05-18 11:17:10 +0900

    fix: ui architecture

---
+++ ui/components/SelectBox.tsx
@@ -0,0 +1,9 @@
+const SelectBox = () => {
+  return (
+    <>
+      <></>
+    </>
+  );
+};
+
+export default SelectBox;
---
+++ ui/components/Transaction.tsx
@@ -0,0 +1,9 @@
+const Transaction = () => {
+  return (
+    <>
+      <></>
+    </>
+  );
+};
+
+export default Transaction;
