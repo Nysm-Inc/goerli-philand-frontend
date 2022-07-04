d493827e96c96bee03e1daf8e6e27a536a3edc35
oji3 <oji3xx@gmail.com>
2022-07-04 13:38:08 +0900

    tx modal

--- ui/components/transaction/ConfirmTx.tsx
+++ ui/components/transaction/ConfirmTx.tsx
@@ -1,22 +1,25 @@
-import { FC } from "react";
-import { Center, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, Text, VStack } from "@chakra-ui/react";
+import Image from "next/image";
+import { FC, useContext } from "react";
+import { Box, Text, VStack } from "@chakra-ui/react";
 import { Tx } from "~/types/wagmi";
+import { Modal, ModalHeader } from "~/ui/components";
+import { AppContext } from "~/contexts";
 
 const ConfirmTx: FC<{ txs: Tx[] }> = ({ txs }) => {
+  const { colorMode } = useContext(AppContext);
   return (
-    <Modal isOpen={txs.some((tx) => tx.tmpStatus === "loading")} onClose={() => {}} isCentered closeOnOverlayClick>
-      <ModalOverlay />
-      <ModalContent border="2px solid" borderColor="black" borderRadius="none" minW="480px" minH="480px" maxW="480px" maxH="480px">
-        <ModalHeader>=======================================</ModalHeader>
-        <ModalBody w="full" h="full">
-          <Center w="full" h="full">
-            <VStack>
-              <Text>WAITING FOR</Text>
-              <Text>CONFIRMATION</Text>
-            </VStack>
-          </Center>
-        </ModalBody>
-      </ModalContent>
+    <Modal w="456px" h="532px" isOpen={txs.some((tx) => tx.tmpStatus === "loading")} onClose={() => {}}>
+      <ModalHeader buttons={[]} />
+      <VStack spacing="16px">
+        <Image src={`/icons/${colorMode === "light" ? "loader" : "loader_dark"}.svg`} width="144px" height="144px" />
+        <Box h="8px" />
+        <Text color="white" textStyle="headline" textAlign="center">
+          WAITING FOR CONFIRMATION
+        </Text>
+        <Text color="#808080" textStyle="paragraph-2" textAlign="center">
+          Confirm this transaction in your wallet
+        </Text>
+      </VStack>
     </Modal>
   );
 };
