47c9f4d2f879d461a3d0587acc9c17fbf54c0586
oji3 <oji3xx@gmail.com>
2022-07-20 14:41:59 +0900

    empty collection

--- ui/features/collection/index.tsx
+++ ui/features/collection/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { FC, useContext, useEffect, useState } from "react";
 import { TransactionResponse } from "@ethersproject/providers";
-import { Box, SimpleGrid, Text, useBoolean, VStack } from "@chakra-ui/react";
+import { Box, Center, SimpleGrid, Text, useBoolean, VStack } from "@chakra-ui/react";
 import { BalanceObject } from "~/types";
 import { objectMetadataList } from "~/types/object";
 import { Icon, IconButton, Modal, ModalBody, ModalFooter, ModalHeader, QuantityInput, ModalFooterButton } from "~/ui/components";
@@ -66,43 +66,55 @@ const Collection: FC<{
         ]}
       />
       <ModalBody>
-        <SimpleGrid columns={3} spacing="8px">
-          {items.map((item, i) => (
-            <VStack key={i} height="320px" p="16px" borderRadius="16px" bgColor={colorMode === "light" ? "#FFFFFF" : "#1A1A1A"}>
-              <Box w="100%" minH="144px" maxH="144px" position="relative">
-                <Image src={objectMetadataList[item.contract][item.tokenId].image_url} layout="fill" objectFit="contain" />
-              </Box>
-              <Text textStyle="label-2" color="#808080">
-                OWNED: {item.amount}
+        {items.length > 0 ? (
+          <SimpleGrid columns={3} spacing="8px">
+            {items.map((item, i) => (
+              <VStack key={i} height="320px" p="16px" borderRadius="16px" bgColor={colorMode === "light" ? "#FFFFFF" : "#1A1A1A"}>
+                <Box w="100%" minH="144px" maxH="144px" position="relative">
+                  <Image src={objectMetadataList[item.contract][item.tokenId].image_url} layout="fill" objectFit="contain" />
+                </Box>
+                <Text textStyle="label-2" color="#808080">
+                  OWNED: {item.amount}
+                </Text>
+                <Text textStyle="headline-2" color={colorMode === "light" ? "#1A1A1A" : "#FFFFFF"}>
+                  {objectMetadataList[item.contract][item.tokenId].name}
+                </Text>
+                <Box h="8px" />
+                {!isEdit && (
+                  <>
+                    {isApproved[item.contract] ? (
+                      <>
+                        {item.contract !== WALLPAPER_CONTRACT_ADDRESS && (
+                          <QuantityInput
+                            defaultText="+ Deposit"
+                            num={item.select}
+                            balance={item.amount}
+                            handleClickMinus={() => minus(i)}
+                            handleClickPlus={() => plus(i)}
+                          />
+                        )}
+                      </>
+                    ) : (
+                      <Text textStyle="label-1" color="#8080FF" cursor="pointer" onClick={onOpenPermissions}>
+                        + Deposit
+                      </Text>
+                    )}
+                  </>
+                )}
+              </VStack>
+            ))}
+          </SimpleGrid>
+        ) : (
+          // todo
+          <Center w="calc(784px + 24px)" h="calc(532px + 24px)" borderRadius="16px" bgColor={colorMode === "light" ? "#FFFFFF" : "#1A1A1A"}>
+            <VStack spacing="32px">
+              <Image src="/assets/empty-collection.png" width="360px" height="240px" />
+              <Text w="300px" h="40px" color="#808080" textStyle="paragraph-2" textAlign="center">
+                {"Manage Objects & Wallpaper collected from Shops and Quests"}
               </Text>
-              <Text textStyle="headline-2" color={colorMode === "light" ? "#1A1A1A" : "#FFFFFF"}>
-                {objectMetadataList[item.contract][item.tokenId].name}
-              </Text>
-              <Box h="8px" />
-              {!isEdit && (
-                <>
-                  {isApproved[item.contract] ? (
-                    <>
-                      {item.contract !== WALLPAPER_CONTRACT_ADDRESS && (
-                        <QuantityInput
-                          defaultText="+ Deposit"
-                          num={item.select}
-                          balance={item.amount}
-                          handleClickMinus={() => minus(i)}
-                          handleClickPlus={() => plus(i)}
-                        />
-                      )}
-                    </>
-                  ) : (
-                    <Text textStyle="label-1" color="#8080FF" cursor="pointer" onClick={onOpenPermissions}>
-                      + Deposit
-                    </Text>
-                  )}
-                </>
-              )}
             </VStack>
-          ))}
-        </SimpleGrid>
+          </Center>
+        )}
       </ModalBody>
       {items.some((item) => item.select > 0) && (
         <ModalFooter>
