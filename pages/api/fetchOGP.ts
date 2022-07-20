01cb3028e545549cc38ca868f28c7dfa35a62296
oji3 <oji3xx@gmail.com>
2022-07-21 08:56:54 +0900

    external ogp

--- pages/api/fetchOGP.ts
+++ pages/api/fetchOGP.ts
@@ -1,6 +1,16 @@
 import axios from "axios";
+import { JSDOM } from "jsdom";
 import { NextApiRequest, NextApiResponse } from "next";
 import { FRONTEND_URL, UTILS_API_GATEWAY } from "~/constants";
+
+type OGP = {
+  type: string;
+  title: string;
+  image: string;
+  description: string;
+  url: string;
+  site_name: string;
+};
 
 const fetchOGP = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -20,7 +30,27 @@ const fetchOGP = async (req: NextApiRequest, res: NextApiResponse) => {
       const base64 = Buffer.from(raw.data, "binary").toString("base64");
       res.status(200).json({ ogp: `data:${raw.headers["content-type"]};base64,${base64}` });
     } else {
-      // todo
+      const headers = { "User-Agent": "bot" };
+      const response = await axios.get(u.toString(), { headers });
+
+      const dom = new JSDOM(response.data);
+      const meta = dom.window.document.querySelectorAll("meta");
+
+      const ogp = Array.from(meta)
+        .filter((element) => element.hasAttribute("property"))
+        .reduce((memo, el) => {
+          const property = el.getAttribute("property")?.trim().replace("og:", "");
+          const content = el.getAttribute("content");
+          if (property && content) {
+            return { ...memo, [property]: content };
+          } else {
+            return memo;
+          }
+        }, {} as OGP);
+
+      const raw = await axios.get(ogp.image, { responseType: "arraybuffer" });
+      const base64 = Buffer.from(raw.data, "binary").toString("base64");
+      res.status(200).json({ ogp: `data:${raw.headers["content-type"]};base64,${base64}` });
     }
   } catch (err) {
     res.status(400).json({ err });
