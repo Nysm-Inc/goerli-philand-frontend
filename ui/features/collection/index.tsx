60c8a2c70f88f16191aef59bd0ee6b04f7ac56b8
oji3 <oji3xx@gmail.com>
2022-07-24 20:51:59 +0900

    fix

--- ui/features/collection/index.tsx
+++ ui/features/collection/index.tsx
@@ -72,7 +72,7 @@ const Collection: FC<{
         {items.length > 0 ? (
           <SimpleGrid columns={3} spacing="8px">
             {items.map((item, i) => (
-              <VStack key={i} height="320px" p="16px" borderRadius="16px" bgColor={colorMode === "light" ? "white" : "grey.900"}>
+              <VStack key={i} height="288px" p="16px" borderRadius="16px" bgColor={colorMode === "light" ? "white" : "grey.900"}>
                 <Box w="100%" minH="144px" maxH="144px" position="relative">
                   <Image src={objectMetadataList[item.contract][item.tokenId].image_url} layout="fill" objectFit="contain" />
                 </Box>
