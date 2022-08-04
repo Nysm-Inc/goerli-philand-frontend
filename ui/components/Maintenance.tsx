3cd69cf301d7f831db8f05499ab8718340263c00
oji3.eth <95267643+open3x@users.noreply.github.com>
2022-08-04 14:08:24 +0900

    Maintenance page (#62)

--- ui/components/Maintenance.tsx
+++ ui/components/Maintenance.tsx
@@ -1,11 +1,11 @@
-import { NextPage } from "next";
-import Image from "next/image";
-import { useContext, useEffect } from "react";
 import { Link, Text, useBreakpointValue, VStack } from "@chakra-ui/react";
+import Image from "next/image";
+import { FC, useContext, useEffect } from "react";
+import { TWITTER_URL } from "~/constants";
 import { AppContext } from "~/contexts";
 import { useGame } from "~/hooks/game";
 
-const Custom404: NextPage = () => {
+const Maintenance: FC = () => {
   const { game } = useContext(AppContext);
   const isMobile = useBreakpointValue({ base: true, lg: false });
   useGame({ state: { currentENS: "", isEdit: false, isCreatedPhiland: false, phiObjects: [], wallpaper: undefined } });
@@ -33,7 +33,7 @@ const Custom404: NextPage = () => {
         margin="auto"
         zIndex="default"
         w={isMobile ? "320px" : "512px"}
-        h="440px"
+        h="431px"
         p="32px"
         spacing="32px"
         bgColor="white"
@@ -41,16 +41,16 @@ const Custom404: NextPage = () => {
         borderColor="light.g_orange"
         borderRadius="32px"
       >
-        <Image src="/assets/phi404.png" width="192px" height="216px" />
+        <Image src="/assets/maintenance.png" width="338px" height="192px" />
         <VStack spacing="16px">
           <Text textStyle="headline-1" color="grey.900">
-            Page Not found
+            Maintenance
           </Text>
           <Text textStyle="paragraph-1" color="grey.500">
-            The requested URL was not found on the server.
+            Please wait for a while until the recovery. New updates and announcements.
           </Text>
-          <Link color="primary.500" textStyle="paragraph-1" href="/">
-            Back to Home
+          <Link color="primary.500" textStyle="paragraph-1" href={TWITTER_URL} isExternal>
+            Follow on Twitter
           </Link>
         </VStack>
       </VStack>
@@ -58,4 +58,4 @@ const Custom404: NextPage = () => {
   );
 };
 
-export default Custom404;
+export default Maintenance;
