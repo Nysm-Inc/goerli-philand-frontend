c34ead37c64901d89842da6abb941ecd3439bdd2
oji3 <oji3xx@gmail.com>
2022-08-05 23:34:21 +0900

    fix

--- connectors/index.ts
+++ connectors/index.ts
@@ -1,10 +1,16 @@
 import { createClient, configureChains, chain } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
-import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { publicProvider } from "wagmi/providers/public";
+import { connectorsForWallets, wallet } from "@rainbow-me/rainbowkit";
 
 const alchemyId = process.env.ALCHEMY_ID;
-const { chains, provider } = configureChains([chain.polygonMumbai], [alchemyProvider({ alchemyId })]);
-const { connectors } = getDefaultWallets({ appName: "Phi", chains });
+const { chains, provider } = configureChains([chain.polygonMumbai], [alchemyProvider({ alchemyId }), publicProvider()]);
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [wallet.metaMask({ chains }), wallet.coinbase({ appName: "phi", chains }), wallet.walletConnect({ chains })],
+  },
+]);
 const client = createClient({ autoConnect: true, connectors: connectors, provider });
 
 export { client, chains };
