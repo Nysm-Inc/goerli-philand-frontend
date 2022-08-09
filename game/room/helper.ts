3653f6721157548b813baabac418e1aae08a4f6b
oji3.eth <95267643+open3x@users.noreply.github.com>
2022-08-09 18:55:35 +0900

    Fix: Land Size (#90)

--- game/room/helper.ts
+++ game/room/helper.ts
@@ -1,8 +1,11 @@
 import { Point } from "pixi.js";
-import { LAND_OFFSET_Y, LAND_W, ROOM_TILE_N, TILE_H, TILE_W } from "~/constants";
+import { LAND_OFFSET_Y, LAND_W, ROOM_TILE_N, TILE_H, TILE_W, WALLPAPER_BORDER_OFFSET } from "~/constants";
 
 export const tileToLocal = (tileX: number, tileY: number): Point => {
-  return new Point((tileX - tileY) * (TILE_W / 2) + (LAND_W / 2 - TILE_W / 2), (tileX + tileY) * (TILE_H / 2) + LAND_OFFSET_Y);
+  return new Point(
+    (tileX - tileY) * (TILE_W / 2) + (LAND_W / 2 - TILE_W / 2),
+    (tileX + tileY) * (TILE_H / 2) + LAND_OFFSET_Y + WALLPAPER_BORDER_OFFSET
+  );
 };
 
 export const itemToLocal = (tileX: number, tileY: number, sizeX: number, sizeY: number, spriteHeight: number): Point => {
