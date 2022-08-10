c917abf5dc1632090acf1e5ef72e0279cfd8af6e
oji3 <oji3xx@gmail.com>
2022-08-10 14:59:17 +0900

    fix

--- ui/components/ENSNotFound.tsx
+++ ui/components/ENSNotFound.tsx
@@ -26,7 +26,7 @@ const ENSNotFound: FC = () => {
         ]}
       />
       <VStack mt="24px" spacing="32px">
-        <Image src="/icons/ens.svg" width="128px" height="128px" priority />
+        <Image src="/icons/ens.png" width="128px" height="128px" priority quality={100} />
         <VStack spacing="8px">
           <Text textStyle="headline-1" color={colorMode === "light" ? "grey.900" : "white"}>
             ENS Not Found
