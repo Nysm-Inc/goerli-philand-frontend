9d23dc981577c9f14360b2d862c9018d4ed153e6
oji3 <oji3xx@gmail.com>
2022-08-10 13:55:57 +0900

    fix

--- hooks/object/useBalances.tsx
+++ hooks/object/useBalances.tsx
@@ -2,40 +2,35 @@ import { BigNumber } from "ethers";
 import { useContractRead } from "wagmi";
 import { objectMetadataList } from "~/types/object";
 import { BalanceObject, ContractAbis, ObjectContractAddress, WallpaperContractAddress } from "~/types";
+import { useMemo } from "react";
 
 const useBalances = (contract: ObjectContractAddress | WallpaperContractAddress, account?: string, disabled?: boolean): BalanceObject[] => {
+  const metadata = useMemo(() => Object.values(objectMetadataList[contract]), [contract]);
   const { data } = useContractRead({
     addressOrName: contract,
     contractInterface: ContractAbis[contract],
     functionName: "balanceOfBatch",
-    args: account
-      ? [
-          Object.keys(objectMetadataList[contract]).map(() => account),
-          Object.values(objectMetadataList[contract]).map((metadata) => metadata.tokenId),
-        ]
-      : null,
+    args: account ? [metadata.map(() => account), metadata.map((meta) => meta.tokenId)] : null,
     watch: true,
     enabled: !!account && !disabled,
   });
 
   return data
-    ? data.reduce((memo, balance, i) => {
-        const amount = BigNumber.from(balance).toNumber();
+    ? metadata.reduce((memo, meta, i) => {
+        const amount = BigNumber.from(data[i]).toNumber();
         if (amount > 0) {
           return [
             ...memo,
             {
               contract: contract,
-              // todo
-              // tokenId: i + 1,
-              tokenId: Object.values(objectMetadataList[contract]).find((v, idx) => i === idx)?.tokenId,
-              amount: BigNumber.from(balance).toNumber(),
+              tokenId: meta.tokenId,
+              amount: amount,
             },
           ];
         } else {
           return memo;
         }
-      }, [])
+      }, [] as BalanceObject[])
     : [];
 };
 
