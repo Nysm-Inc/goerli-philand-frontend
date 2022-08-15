18e83de340633dd366246776ed617e3a896d4b26
oji3.eth <95267643+open3x@users.noreply.github.com>
2022-08-15 16:10:30 +0900

    Dev (#121)

--- pages/[ens]/index.tsx
+++ pages/[ens]/index.tsx
@@ -1,19 +1,18 @@
 import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
-import { FC, useContext } from "react";
+import { FC, useMemo } from "react";
 import axios from "axios";
 import { useBreakpointValue } from "@chakra-ui/react";
-import { AppContext } from "~/contexts";
 import { UTILS_API_GATEWAY } from "~/constants";
-import { useWallpaper, useViewPhiland } from "~/hooks/map";
+import { PhiObject, Wallpaper } from "~/types";
 import useGame from "~/hooks/game/useGame";
 import useClouds from "~/hooks/game/useClouds";
+import { useWallpaper, useViewPhiland } from "~/hooks/map";
 import { useCreatePhiland } from "~/hooks/registry";
 import Dev from "~/ui/components/Dev";
 import Header from "~/ui/components/Header";
 import HeaderMd from "~/ui/components/HeaderMd";
 import LandNotFound from "~/ui/components/LandNotFound";
-import { PhiObject, Wallpaper } from "~/types";
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   try {
@@ -36,14 +35,13 @@ const Philand: FC<{
 };
 
 const Index: NextPage = () => {
-  const { game } = useContext(AppContext);
   const router = useRouter();
   const ens = decodeURI(router.asPath).substring(1);
   const isMobile = useBreakpointValue({ base: true, lg: false }, { ssr: false });
 
   const [{ isCreated, isFetched }] = useCreatePhiland("", ens);
   const { phiObjects } = useViewPhiland(ens);
-  const isPhilandCreated = isCreated || phiObjects.length > 0;
+  const isPhilandCreated = useMemo(() => isCreated || phiObjects.length > 0, [isCreated, phiObjects.length]);
   const wallpaper = useWallpaper(ens);
   useClouds(isMobile);
 
