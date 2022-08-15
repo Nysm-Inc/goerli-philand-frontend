a79c94c2c154d07ba638e2a93535bd576bd61151
oji3 <oji3xx@gmail.com>
2022-08-15 15:39:32 +0900

    fix

--- hooks/ens/subdomain.ts
+++ hooks/ens/subdomain.ts
@@ -7,8 +7,8 @@ type CacheSubdomain = {
 export const getPhiSubdomain = (labelhash: string): string => {
   try {
     // @ts-ignore
-    const subdomains: CacheSubdomain = JSON.parse(localStorage.getItem(PHI_SUBDOMAINS_KEY));
-    return subdomains[labelhash];
+    const subdomains: CacheSubdomain | null = JSON.parse(localStorage.getItem(PHI_SUBDOMAINS_KEY));
+    return subdomains ? subdomains[labelhash] : "";
   } catch (err) {
     console.error(err);
     return "";
