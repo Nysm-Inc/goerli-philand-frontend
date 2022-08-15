fa64ee877aa3ba17a591bf0be53ef03d9220b885
oji3 <oji3xx@gmail.com>
2022-08-15 18:10:39 +0900

    fix

--- hooks/wallpaper/index.tsx
+++ hooks/wallpaper/index.tsx
@@ -1,10 +1,12 @@
 import { useContext, useEffect } from "react";
 import { useDeprecatedContractWrite, useWaitForTransaction } from "wagmi";
+import { BigNumber, utils } from "ethers";
 import type { TransactionResponse } from "@ethersproject/providers";
 import { WALLPAPER_CONTRACT_ADDRESS } from "~/constants";
 import WallpaperAbi from "~/abi/wallpaper.json";
 import { AppContext } from "~/contexts";
 import { getFastestGasWei } from "~/utils/gas";
+import { objectMetadataList } from "~/types/object";
 
 const useGetWallpaper = (): { batchWallPaper: (tokenIds: number[]) => Promise<TransactionResponse | undefined> } => {
   const { addTx } = useContext(AppContext);
@@ -32,7 +34,19 @@ const useGetWallpaper = (): { batchWallPaper: (tokenIds: number[]) => Promise<Tr
     batchWallPaper: async (tokenIds: number[]) => {
       const calldata = [tokenIds];
       const overrides = await getFastestGasWei();
-      return writeAsync({ args: calldata, overrides });
+      return writeAsync({
+        args: calldata,
+        overrides: {
+          ...overrides,
+          value: tokenIds
+            .reduce((sum, tokenId) => {
+              const metadata = objectMetadataList[WALLPAPER_CONTRACT_ADDRESS][tokenId];
+              const wei = utils.parseEther(metadata.price.toString());
+              return sum.add(wei);
+            }, BigNumber.from(0))
+            .toString(),
+        },
+      });
     },
   };
 };
