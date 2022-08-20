92c99f3169073c8b7815445a6f252a06b381a254
oji3 <oji3xx@gmail.com>
2022-08-20 21:17:30 +0900

    fix

--- ui/components/common/MenuList.tsx
+++ ui/components/common/MenuList.tsx
@@ -23,77 +23,70 @@ const MenuList: FC<{
   const { colorMode } = useContext(AppContext);
 
   return (
-    <>
-      <ChakraMenuList
-        maxHeight="240px"
-        borderRadius="12px"
-        p="8px"
-        overflowY="scroll"
-        boxShadow="md"
-        minW="0"
-        w={w}
-        maxH={maxH}
-        //
-        border={colorMode === "light" ? "1px solid" : "none"}
-        borderColor="light.g_orange"
-        bgColor={colorMode === "light" ? "white" : "grey.900"}
-      >
-        {options.map((option, i) => (
-          <Fragment key={i}>
-            <MenuItem
-              //
-              key={i}
-              w="100%"
-              h={option.image ? "48px" : "32px"}
-              p="8px"
-              textAlign="left"
-              borderRadius="6px"
-              isFocusable={false}
-              onClick={() => {
-                if (option.onClick) option.onClick();
-                else if (onClick) onClick(option.value);
-              }}
-              //
-              bgColor={colorMode === "light" ? "white" : "grey.900"}
-              _hover={{
-                bgColor: colorMode === "light" ? "light.lg_orange40" : "dark.grey700",
-              }}
-              _active={{
-                bgColor: colorMode === "light" ? "white" : "dark.grey700",
-              }}
-              _focus={{
-                bgColor: colorMode === "light" ? "white" : "dark.grey700",
-              }}
-            >
-              {option.image && (
-                <Center mr="16px">
-                  <Image src={option.image} width="64px" height="32px" alt="" />
-                </Center>
-              )}
-              <Text
-                textStyle="button-2"
-                w="calc(100% - 16px)"
-                whiteSpace="nowrap"
-                overflow="hidden"
-                textOverflow="ellipsis"
-                textColor={option.textColor || (colorMode === "light" ? "grey.900" : "white")}
-              >
-                {option.label}
-              </Text>
-              <Box w="8px" />
-              <Center w="24px">
-                {value === option.value && <Icon name="check" color={colorMode === "light" ? "grey.900" : "white"} />}
+    <ChakraMenuList
+      maxHeight="240px"
+      borderRadius="12px"
+      p="8px"
+      overflowY="scroll"
+      boxShadow="md"
+      minW="0"
+      w={w}
+      maxH={maxH}
+      border={colorMode === "light" ? "1px solid" : "none"}
+      borderColor="light.g_orange"
+      bgColor={colorMode === "light" ? "white" : "grey.900"}
+    >
+      {options.map((option, i) => (
+        <Fragment key={i}>
+          <MenuItem
+            key={i}
+            w="100%"
+            h={option.image ? "48px" : "32px"}
+            p="8px"
+            textAlign="left"
+            borderRadius="6px"
+            isFocusable={false}
+            onClick={() => {
+              if (option.onClick) option.onClick();
+              else if (onClick) onClick(option.value);
+            }}
+            bgColor={colorMode === "light" ? "white" : "grey.900"}
+            _hover={{
+              bgColor: colorMode === "light" ? "light.lg_orange40" : "dark.grey700",
+            }}
+            _active={{
+              bgColor: colorMode === "light" ? "white" : "dark.grey700",
+            }}
+            _focus={{
+              bgColor: colorMode === "light" ? "white" : "dark.grey700",
+            }}
+          >
+            {option.image && (
+              <Center mr="16px">
+                <Image src={option.image} width="64px" height="32px" alt="" />
               </Center>
-            </MenuItem>
-            {option.divider && (
-              <Box p="8px 0px" w="100%" h="17px">
-                <Box w="100%" h="1px" bgColor={colorMode === "light" ? "grey.100" : "dark.grey700"} />
-              </Box>
             )}
-          </Fragment>
-        ))}
-      </ChakraMenuList>
-    </>
+            <Text
+              textStyle="button-2"
+              w="calc(100% - 16px)"
+              whiteSpace="nowrap"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              textColor={option.textColor || (colorMode === "light" ? "grey.900" : "white")}
+            >
+              {option.label}
+            </Text>
+            <Box w="8px" />
+            <Center w="24px">{value === option.value && <Icon name="check" color={colorMode === "light" ? "grey.900" : "white"} />}</Center>
+          </MenuItem>
+          {option.divider && (
+            <Box p="8px 0px" w="100%" h="17px">
+              <Box w="100%" h="1px" bgColor={colorMode === "light" ? "grey.100" : "dark.grey700"} />
+            </Box>
+          )}
+        </Fragment>
+      ))}
+    </ChakraMenuList>
   );
 };
 
