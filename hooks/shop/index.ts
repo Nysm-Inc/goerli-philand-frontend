fe505d50f003c139d26c0f303707f40b3e42e3b5
oji3 <oji3xx@gmail.com>
2022-08-21 10:19:33 +0900

    fix

---
+++ hooks/shop/index.ts
@@ -0,0 +1,62 @@
+import { useContext, useEffect } from "react";
+import { useDeprecatedContractWrite, useWaitForTransaction } from "wagmi";
+import { BigNumber, utils } from "ethers";
+import type { TransactionResponse } from "@ethersproject/providers";
+import { PREMIUM_OBJECT_CONTRACT_ADDRESS, WALLPAPER_CONTRACT_ADDRESS } from "~/constants";
+import PremiumObjectAbi from "~/abi/premiumobject.json";
+import { objectMetadataList } from "~/types/object";
+import { AppContext } from "~/contexts";
+import { getFastestGasWei } from "~/utils/gas";
+
+const useBuyObjects = (
+  address?: string
+): {
+  buyObjects: (fTokenIds: number[], pTokenIds: number[], wTokenIds: number[]) => Promise<TransactionResponse | undefined>;
+} => {
+  const { addTx } = useContext(AppContext);
+  const {
+    data,
+    writeAsync,
+    status: tmpStatus,
+  } = useDeprecatedContractWrite({
+    addressOrName: PREMIUM_OBJECT_CONTRACT_ADDRESS,
+    contractInterface: PremiumObjectAbi,
+    functionName: "shopBuyObject",
+  });
+  const { status } = useWaitForTransaction({ hash: data?.hash || "" });
+
+  useEffect(() => {
+    addTx({
+      hash: data?.hash,
+      tmpStatus,
+      status,
+      action: "Purchasing Objects",
+    });
+  }, [tmpStatus, status]);
+
+  return {
+    buyObjects: async (fTokenIds: number[], pTokenIds: number[], wTokenIds: number[]) => {
+      const calldata = [address, fTokenIds, pTokenIds, wTokenIds];
+      const overrides = await getFastestGasWei();
+
+      const pValue = pTokenIds.reduce((sum, tokenId) => {
+        const metadata = objectMetadataList[PREMIUM_OBJECT_CONTRACT_ADDRESS][tokenId];
+        const wei = utils.parseEther(metadata.price.toString());
+        return sum.add(wei);
+      }, BigNumber.from(0));
+
+      const wValue = wTokenIds.reduce((sum, tokenId) => {
+        const metadata = objectMetadataList[WALLPAPER_CONTRACT_ADDRESS][tokenId];
+        const wei = utils.parseEther(metadata.price.toString());
+        return sum.add(wei);
+      }, BigNumber.from(0));
+
+      return writeAsync({
+        args: calldata,
+        overrides: { ...overrides, value: pValue.add(wValue).toString() },
+      });
+    },
+  };
+};
+
+export default useBuyObjects;
