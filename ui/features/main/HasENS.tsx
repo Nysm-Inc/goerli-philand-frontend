ff2ce1e38c149eae8f256dae124d1e1b010e715e
oji3 <oji3xx@gmail.com>
2022-08-22 22:00:40 +0900

    fix

--- ui/features/main/HasENS.tsx
+++ ui/features/main/HasENS.tsx
@@ -15,7 +15,7 @@ const HasENS: FC<{ address: string; currentENS: string; domains: string[]; switc
   const { createPhiland } = useCreatePhiland(address, currentENS);
   const { changePhilandOwner } = useChangePhilandOwner(address, currentENS);
   const { owner, phiObjects } = useViewPhiland(currentENS);
-  const isCreatedPhiland = useMemo(() => owner === address && phiObjects.length > 0, [owner, address, phiObjects.length]);
+  const isCreatedPhiland = useMemo(() => owner === address, [owner, address]);
 
   return (
     <>
