889825655f1ea451342779c68bff5f8bbae74439
oji3.eth <95267643+open3x@users.noreply.github.com>
2022-08-26 15:49:18 +0900

    fix: sentry capture console error #171

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,7 +1,9 @@
 import * as Sentry from "@sentry/nextjs";
+import { CaptureConsole } from "@sentry/integrations";
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NEXT_PUBLIC_APP_ENV,
   tracesSampleRate: 1.0,
+  integrations: [new CaptureConsole({ levels: ["error"] })],
 });
