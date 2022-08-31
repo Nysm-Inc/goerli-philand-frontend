8e84e263a4148cc94220353173821a4052791689
oji3 <oji3xx@gmail.com>
2022-08-31 22:22:04 +0900

    fix

--- utils/sentry.ts
+++ utils/sentry.ts
@@ -3,7 +3,6 @@ import { captureException } from "@sentry/nextjs";
 export const captureError = (err: string) => {
   if (process.env.NEXT_PUBLIC_APP_ENV === "local") {
     console.error(err);
-    captureException(new Error(err));
   } else {
     captureException(new Error(err));
   }
