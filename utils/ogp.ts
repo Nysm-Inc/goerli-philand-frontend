949440c5674a704379d52e24526f5dbd94e41cde
oji3 <oji3xx@gmail.com>
2022-09-01 17:47:12 +0900

    fix

--- utils/ogp.ts
+++ utils/ogp.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 import { UTILS_API_GATEWAY } from "~/constants";
 
 export const updateOGP = async (ens: string | null | undefined, dataurl: string) => {
-  const body = {
-    name: ens + ".png",
-    file: dataurl,
-  };
-  return axios.post(UTILS_API_GATEWAY + "/images/upload", JSON.stringify(body));
+  const body = { name: ens + ".png", file: dataurl };
+  return axios.post(UTILS_API_GATEWAY + "/images/upload", body);
 };
