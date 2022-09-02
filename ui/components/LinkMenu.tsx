5db2dcf0f583892cba4baffa27dbb8d3fa7f52ab
oji3 <oji3xx@gmail.com>
2022-09-02 17:53:27 +0900

    fix

--- ui/components/LinkMenu.tsx
+++ ui/components/LinkMenu.tsx
@@ -82,10 +82,15 @@ const LinkMenu: FC<{
                   w="120px"
                   color="green"
                   onClick={() => {
-                    event({ action: "conversion_set_link" });
-                    onChange(state.id, { title: input.title, url: input.url });
-                    onClose(state.id);
-                    onBack();
+                    try {
+                      const url = new URL(input.url);
+                      if (url.protocol !== "https:") return;
+
+                      event({ action: "conversion_set_link" });
+                      onChange(state.id, { title: input.title, url: url.toString() });
+                      onClose(state.id);
+                      onBack();
+                    } catch {}
                   }}
                 >
                   <Icon name="check" />
