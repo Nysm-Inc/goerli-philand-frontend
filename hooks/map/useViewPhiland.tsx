91472d85e496052785c700c1ed07e02c7427eb31
oji3 <oji3xx@gmail.com>
2022-09-04 15:19:41 +0900

    fix

--- hooks/map/useViewPhiland.tsx
+++ hooks/map/useViewPhiland.tsx
@@ -21,41 +21,33 @@ const useViewPhiland = (
     args: ens ? [ens.slice(0, -4)] : null,
     watch: true,
   });
-  const { data: links } = useContractRead({
-    addressOrName: MAP_CONTRACT_ADDRESS,
-    contractInterface: MapAbi,
-    functionName: "viewLinks",
-    args: ens ? [ens.slice(0, -4)] : null,
-    watch: true,
-  });
 
   return {
     // @ts-ignore
     owner: owner || nullAddress,
     isFetchedOwner,
-    phiObjects:
-      objects && links
-        ? objects.reduce((memo, object, idx) => {
-            const tokenId = BigNumber.from(object[1]).toNumber();
-            if (tokenId > 0) {
-              return [
-                ...memo,
-                {
-                  removeIdx: idx,
-                  contractAddress: object[0],
-                  tokenId: tokenId,
-                  xStart: BigNumber.from(object[2]).toNumber(),
-                  yStart: BigNumber.from(object[3]).toNumber(),
-                  xEnd: BigNumber.from(object[4]).toNumber(),
-                  yEnd: BigNumber.from(object[5]).toNumber(),
-                  link: { title: links[idx]?.[0], url: links[idx]?.[1] },
-                } as PhiObject,
-              ];
-            } else {
-              return memo;
-            }
-          }, [])
-        : [],
+    phiObjects: objects
+      ? objects.reduce((memo, object, idx) => {
+          const tokenId = BigNumber.from(object[1]).toNumber();
+          if (tokenId > 0) {
+            return [
+              ...memo,
+              {
+                removeIdx: idx,
+                contractAddress: object[0],
+                tokenId: tokenId,
+                xStart: BigNumber.from(object[2]).toNumber(),
+                yStart: BigNumber.from(object[3]).toNumber(),
+                xEnd: BigNumber.from(object[4]).toNumber(),
+                yEnd: BigNumber.from(object[5]).toNumber(),
+                link: { title: object[6]?.title || "", url: object[6]?.url || "" },
+              } as PhiObject,
+            ];
+          } else {
+            return memo;
+          }
+        }, [])
+      : [],
   };
 };
 
