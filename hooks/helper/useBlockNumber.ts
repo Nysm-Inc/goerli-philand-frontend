1c2e6c66dac564e71f80718117d1179f1e1cc870
oji3 <oji3xx@gmail.com>
2022-09-06 19:27:00 +0900

    fix

--- hooks/helper/useBlockNumber.ts
+++ hooks/helper/useBlockNumber.ts
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import useInterval from "./useInterval";
 
-// for testnet
-const useBlockNumber = (watch?: boolean): number => {
+const mumbaiBlockTime = 5000;
+
+const useBlockNumber = (): number => {
   // mainnet
   // const { data: blockNumber } = useBlockNumber({ watch: !!watch });
+
   // mumbai
   const [blockNumber, setBlockNumber] = useState(0);
-
-  useInterval(() => {
-    if (watch) setBlockNumber((prev) => prev++);
-  }, 5000);
+  useInterval(() => setBlockNumber((prev) => prev++), mumbaiBlockTime);
 
   return blockNumber;
 };
