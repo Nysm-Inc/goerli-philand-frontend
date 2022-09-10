b958b2212fa07562c733c90c7abe7c6c771632a5
oji3 <oji3xx@gmail.com>
2022-09-10 16:18:37 +0900

    fix

--- game/ui/UIManager.ts
+++ game/ui/UIManager.ts
@@ -1,12 +1,12 @@
 import { ObjectContractAddress, PhiLink } from "~/types";
 
 export type UIManagerHandler = {
-  onOpenActionMenu: (id: string, globalX: number, globalY: number) => void;
-  onOpenWallpaperMenu: (globalX: number, globalY: number) => void;
-  onChangeLinkMenu: (id: string, link: PhiLink) => void;
-  onPlaceFromLand: (contract: ObjectContractAddress, tokenId: number) => void;
-  onChangeScaled: (scaled: number) => void;
-  onPushRouter: (path: string) => void;
+  onOpenActionMenu?: (id: string, globalX: number, globalY: number) => void;
+  onOpenWallpaperMenu?: (globalX: number, globalY: number) => void;
+  onChangeLinkMenu?: (id: string, link: PhiLink) => void;
+  onPlaceFromLand?: (contract: ObjectContractAddress, tokenId: number) => void;
+  onChangeScaled?: (scaled: number) => void;
+  onPushRouter?: (path: string) => void;
 };
 
 export default class UIManager {
@@ -27,11 +27,11 @@ export default class UIManager {
   }
 
   loadUIHandler(handler: UIManagerHandler) {
-    this.onOpenActionMenu = handler.onOpenActionMenu;
-    this.onOpenWallpaperMenu = handler.onOpenWallpaperMenu;
-    this.onChangeLinkMenu = handler.onChangeLinkMenu;
-    this.onPlaceFromLand = handler.onPlaceFromLand;
-    this.onChangeScaled = handler.onChangeScaled;
-    this.onPushRouter = handler.onPushRouter;
+    this.onOpenActionMenu = handler.onOpenActionMenu || (() => {});
+    this.onOpenWallpaperMenu = handler.onOpenWallpaperMenu || (() => {});
+    this.onChangeLinkMenu = handler.onChangeLinkMenu || (() => {});
+    this.onPlaceFromLand = handler.onPlaceFromLand || (() => {});
+    this.onChangeScaled = handler.onChangeScaled || (() => {});
+    this.onPushRouter = handler.onPushRouter || (() => {});
   }
 }
