b4a6727e7811744a729864d9f22f93f99325c92c
oji3 <oji3xx@gmail.com>
2022-11-19 11:07:29 +0800

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 import type { NextPage } from "next";
 import { useContext, useEffect } from "react";
-import { useBreakpointValue, useDisclosure } from "@chakra-ui/react";
+import { useBreakpointValue } from "@chakra-ui/react";
 import { AppContext } from "~/contexts";
 import useClouds from "~/hooks/game/useClouds";
-import useScore from "~/hooks/leaderboard/score";
 import Main from "~/ui/features/main";
-import Leaderboard, { LeaderboardButton, LeaderboardButtonMd } from "~/ui/features/leaderboard";
 import Dev from "~/ui/components/Dev";
 import Mobile from "~/ui/components/Mobile";
 
 const Index: NextPage = () => {
   const { game } = useContext(AppContext);
   const isMobile = useBreakpointValue({ base: true, lg: false }, { ssr: false });
-  const { isOpen: isOpenLeaderboard, onOpen: onOpenLeaderboard, onClose: onCloseLeaderboard } = useDisclosure();
-  const { topScoreList } = useScore(undefined, isOpenLeaderboard);
 
   useClouds(isMobile);
   useEffect(() => {
@@ -26,18 +22,7 @@ const Index: NextPage = () => {
   return (
     <>
       <Dev />
-      <Leaderboard isMobile={isMobile} topScoreList={topScoreList} isOpen={isOpenLeaderboard} onClose={onCloseLeaderboard} />
-      {isMobile ? (
-        <>
-          <LeaderboardButtonMd shadow onOpen={onOpenLeaderboard} />
-          <Mobile />
-        </>
-      ) : (
-        <>
-          <LeaderboardButton shadow onOpen={onOpenLeaderboard} />
-          <Main />
-        </>
-      )}
+      {isMobile ? <Mobile /> : <Main />}
     </>
   );
 };
